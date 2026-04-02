ec087ee16c3b5978193aedffe82463eea382f2ff
Angelo Vargas <avargasc9@correo.uss.cl>
2026-04-02 18:00:42 -0300

    main.py

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 from datetime import datetime
-
+from zoneinfo import ZoneInfo
 import ntplib
 from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/time")
 def get_time():
     try:
         client = ntplib.NTPClient()
-        # Servidor oficial de SHOA
         response = client.request("ntp.shoa.cl", version=3)
 
-        fecha_hora = datetime.fromtimestamp(response.tx_time)
+        fecha_hora = datetime.fromtimestamp(response.tx_time, ZoneInfo("America/Santiago"))
         salida = fecha_hora.strftime("%Y-%m-%d %H:%M:%S")
 
         return {"Hora_Oficial_Chile": salida}
